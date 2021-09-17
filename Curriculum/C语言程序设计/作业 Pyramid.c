5dc8eab420ff456a00aa5926e3a98532335130bd
素章 <88318878+SuZhangs@users.noreply.github.com>
2021-09-17 19:19:14 +0800

    Update 作业 Pyramid.c

--- Curriculum/C语言程序设计/作业 Pyramid.c
+++ Curriculum/C语言程序设计/作业 Pyramid.c
@@ -1,1 +1,24 @@
-
+#include<stdio.h>
+void Pyramid()
+                    //Define a function called Pyramid without return value 
+{
+  int i,j,k;
+  int n;            //Define the height of the pyramid n 
+  printf("Please enter the height of the pyramid\n");
+  scanf("%d",&n); 
+  
+  for(i=1;i<=n;i++) // loop output i-th line
+      {
+      for(k=i;k<=n-1;k++)
+        {
+                    //Circularly output the spaces in the i-th line, the number of cycles i
+          printf(" ");
+        }
+       for(j=1;j<=2*i-1;j++)
+       {
+                    //Circularly output the * in the ith row, and the number of cycles is j
+          printf("*");
+        }
+      printf("\n"); 
+      }
+}
