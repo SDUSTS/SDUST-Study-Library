a855686d3ba271ffce569d4cfbfa07c0cb7fcbf7
SuZhangs <88318878+SuZhangs@users.noreply.github.com>
2022-07-02 09:36:23 +0800

    Add files via upload

---
+++ Altium Designer基本知识介绍.md
@@ -0,0 +1,59 @@
+# Altium Designer 20 AD20
+
+## 快捷键
+
+ctrl + 鼠标 放大缩小
+
+空格键 旋转元件
+
+项目project
+
+* 原理图 schematic
+
+* PCB 
+
+库Library
+
+* 综合库 integrated library
+
+* 原理图库
+
+* PCB库
+
+### 窗口介绍
+
+
+
+### 如何添加库
+
+#### components 组件库
+
+connectors IntLib 连接器综合库
+
+Devices IntLib 元器件综合库
+
+如果没有库。可以安装库。或者自己绘制库。
+
+> 综合库： 每一个器件有自己的原理图和对应的PCB封装
+>
+> 
+
+### 界面大小
+
+#### properties 属性
+
+### 布线
+
+快捷窗口
+
+### 把原理图导入PCB
+
+黑框是布线的区域
+
+* 布局
+
+* 走线
+
+
+
+库》 原理图》生成网络线到达PCB》简单绘制PCB的流程
---
+++ matlab绘图代码.m
@@ -0,0 +1,31 @@
+%% Edit by  MQC  
+%这个代码用于做 moudle 19 的 data1—5的 图 的。
+%方法是 在 工作区 新建一个变量 A  。然后将要处理的某一个data的数据复制粘贴过去（要注意保证一共有偶数行）。
+%然后全选这些代码，右键执行。
+%% 
+[r,c] = size(A);%
+LEDdata  = zeros(r/2,1);
+ Ambientdata= zeros(r/2,1);
+ k = 1;
+ z = 1;
+ 
+ %%  这里的两个循环。意思是分别把A中的奇数行和偶数行放入到两个新的列中。
+for i =2:2:r
+    LEDdata(k,1) =  A (i,1);
+    k = k+1;
+end
+for i =1:2:r 
+    Ambientdata(z,1) = A(i,1);
+    z = z +1 ;
+end
+%%
+x = LEDdata;
+y = Ambientdata;
+plot(x,y)
+%% 
+title('data 4') %这里可以改一下图的名字
+xlabel('LEDdata');
+ylabel('Ambientdata');
+legend('LEDdata');
+%%
+% 这些代码参考一下就好啦
